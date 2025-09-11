789038129802d078ac4e58d7b831ecaec81a4d41
baqdauletd <baqdaulet.dauletov04@gmail.com>
2025-09-11 17:04:46 +0500

    repo firsst draft + test in main

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
-import "todo-list/db"
+import (
+	"fmt"
+	"time"
+	"todo-list/db"
+	"todo-list/models"
+	"todo-list/repo"
+)
 
 func main(){
-	db.Connect()
+	db := db.Connect()
+
+	taskRepo := repo.NewTaskRepo(db)
+
+	dueDate := time.Now().Add(24*time.Hour)
+	task := models.Task{
+		Title: "Learn Go", 
+		Description: "Finish Repo Layer",
+		Completed: false, 
+		DueDate: &dueDate,
+		Priority: "High",
+	}
+	taskRepo.CreateTaskRepo(task)
+	fmt.Println("Task Created")
+
+	found := taskRepo.FindTaskRepo(1)
+	fmt.Println("Found Task:", found)
+
+	tasks := taskRepo.ListTaskRepo()
+	fmt.Println("All Tasks:", tasks)
+
+	found.Completed = true
+	taskRepo.UpdateTaskRepo(found)
+	fmt.Println("Task Updated", found)
+
+	taskRepo.DeleteTaskRepo(1)
+	fmt.Println("Task Deleted")
+
+	tasks1 := taskRepo.ListTaskRepo()
+	fmt.Println("All Tasks:", tasks1)
 }
 
 /* 
---
+++ repo/task_repo.go
@@ -0,0 +1,40 @@
+package repo
+
+import (
+	"todo-list/models"
+
+	"gorm.io/gorm"
+)
+
+type TaskRepo struct{
+	db *gorm.DB
+}
+
+func NewTaskRepo(db *gorm.DB) *TaskRepo{
+	return &TaskRepo{db:db}
+}
+
+func (r *TaskRepo) CreateTaskRepo(tasks models.Task) error{
+	return r.db.Create(&tasks).Error
+}
+
+func (r *TaskRepo) FindTaskRepo(id uint) (*models.Task){
+	var task models.Task
+	r.db.First(&task, "id=?", id)
+
+	return &task
+}
+
+func (r *TaskRepo) ListTaskRepo() (*[]models.Task){
+	var tasks []models.Task
+	r.db.Find(&tasks)
+	return &tasks
+}
+
+func (r *TaskRepo) UpdateTaskRepo(task *models.Task){
+	r.db.Save(&task)
+}
+
+func (r *TaskRepo) DeleteTaskRepo(id uint){
+	r.db.Delete(&models.Task{}, "id=?", id)
+}
